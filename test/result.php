acb67fe3c261126715400e62ed34ad5eaf581a18
GustavsOzolins <35406816+GustavsOzolins@users.noreply.github.com>
2018-09-05 21:37:44 +0300

    Update result.php

--- test/result.php
+++ test/result.php
@@ -67,11 +67,8 @@ if(!empty($_SESSION['name'])){
 
             </div>
         </div>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="js/jquery-1.10.2.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
-
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="js/jquery.validate.min.js"></script>
 
     </body>
