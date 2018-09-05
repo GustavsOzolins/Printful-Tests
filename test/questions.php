451e9388f7cc51647279b0c9e0f3a9b760536b06
GustavsOzolins <35406816+GustavsOzolins@users.noreply.github.com>
2018-09-05 21:36:34 +0300

    Update questions.php

--- test/questions.php
+++ test/questions.php
@@ -22,12 +22,6 @@ if(!empty($_SESSION['name'])){
 		<!-- Bootstrap -->
 		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="css/style.css" rel="stylesheet" media="screen">
-
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-		<script src="../../assets/js/html5shiv.js"></script>
-		<script src="../../assets/js/respond.min.js"></script>
-		<![endif]-->
 		<style>
 			.container {
 				margin-top: 110px;
